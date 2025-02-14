fa5dad58232a986f638e01b132ece021606d1d8a
Mona Lisa <Mukes@LAPTOP-50K23EMT>
2025-02-14 09:58:21 +0530

    Dev1--adding new things Branch1

--- GITProj01/src/main/java/com/nt/UPI/UPIPaymentOperation.java
+++ GITProj01/src/main/java/com/nt/UPI/UPIPaymentOperation.java
@@ -1,9 +1,14 @@
 package com.nt.UPI;
 
+import java.util.Random;
+
 public class UPIPaymentOperation {
 	public String doPayment(String upiId,double amount){
 		return amount +"is transfered to "+upiId;
    }
+	public String addrewards(String txId) {
+		return txId+"has got"+new Random().nextInt(4000)+"reward points have come";
+	}
 	
 }
 
